b75ba416241b47d49232d88e45c571b3ad494624
Ai Anshu <68537640+aianshume@users.noreply.github.com>
2022-03-22 12:08:43 +0530

    Update generateMeta.js

--- scripts/generateMeta.js
+++ scripts/generateMeta.js
@@ -89,18 +89,18 @@ async function getSearchMeta() {
 
 // getSearchMeta();
 
-async function uploadAssets(){
-  cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true
-  });
-  const files = fs.readdirSync(path.join(process.cwd(), "public/assets"));
-  files.forEach(async(img) => {
-    await cloudinary.uploader.upload(`./public/assets/${img}`, {use_filename: true, unique_filename: false}, function(error, result) {console.log(error)})
-  })
-}
+// async function uploadAssets(){
+//   cloudinary.config({ 
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//     api_key: process.env.CLOUDINARY_API_KEY, 
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//     secure: true
+//   });
+//   const files = fs.readdirSync(path.join(process.cwd(), "public/assets"));
+//   files.forEach(async(img) => {
+//     await cloudinary.uploader.upload(`./public/assets/${img}`, {use_filename: true, unique_filename: false}, function(error, result) {console.log(error)})
+//   })
+// }
 
 getSearchMeta();
-uploadAssets();
+// uploadAssets();
