272f17cbee37f40fd8f34fefdb133412bbe3f57a
Ai Anshu <anshumeena1947@gmail.com>
2022-03-26 10:16:27 +0530

    fix: guscus

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -6,12 +6,15 @@ export default function Comment() {
     <Giscus
       key={"Comments"}
       repo="aianshume/codenanshu"
-      repoId="MDEwOlJlcG9zaXRvcnkzODIzMjE5Mjc="
-      category="Q&A"
-      categoryId="DIC_kwDOFsnFB84B-WZk"
+      repoId="R_kgDOHBgQ6Q"
+      category={`Q&A`}
+      categoryId="DIC_kwDOHBgQ6c4COKjd"
       mapping="pathname"
       reactionsEnabled="1"
       emitMetadata="0"
+      inputPosition="bottom"
+      lang="en"
+      loading="lazy"
       theme={useColorModeValue("light", "dark_high_contrast")}
     />
   );
