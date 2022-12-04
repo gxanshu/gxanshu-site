01193f7a762fc66cadf91acb2da6ef499326d8a5
Ai Anshu <anshumeena1947@gmail.com>
2022-12-04 23:11:32 +0530

    just fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,26 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import customTheme from './src/utils/theme.json'
+import customTheme from './src/utils/theme.json';
 
 // https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:3000",
+  site: "http://dev.codenanshu.in",
   markdown: {
     shikiConfig: {
-      theme: customTheme,
-    },
+      theme: customTheme
+    }
   },
-  integrations: [mdx(), tailwind(), alpinejs()],
+  integrations: [mdx(), tailwind(), alpinejs(), sitemap({
+    filter: page => page !== '/admin'
+  }), partytown()]
 });
