031b3ecbd0312f32ce631c51cdf3ba1f8cbeabc6
aianshume <anshumeena1947@gmail.com>
2023-01-23 11:22:57 +0530

    add: heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import customTheme from "./src/utils/theme.json";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { autolinkConfig } from './scripts/rehype-autolink-config';
 
 // https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
@@ -18,16 +20,22 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://codenanshu.in",
-  markdown: {
-    shikiConfig: {
-      theme: customTheme
-    }
-  },
   integrations: [mdx(), tailwind(), alpinejs(), sitemap({
     filter: page => page !== "/admin"
   }), partytown({
     config: {
       forward: ["datalayer.push"]
     }
-  }), preact({compat: true})]
+  }), preact({compat: true})],
+
+  markdown: {
+    gfm: true,
+    shikiConfig: {
+      theme: customTheme
+    },
+    rehypePlugins: [
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+  },
 });
