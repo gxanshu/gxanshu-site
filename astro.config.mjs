3b8f8e67e5b8f8b07dc51823f6a4e00302b28706
aianshume <anshumeena1947@gmail.com>
2023-01-23 23:09:30 +0530

    add: prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,43 +2,34 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import customTheme from "./src/utils/theme.json";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+import preact from "@astrojs/preact";
+import prefetch from "@astrojs/prefetch";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./scripts/rehype-autolink-config";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://codenanshu.in",
-  integrations: [
-    mdx(),
-    tailwind(),
-    sitemap({
-      filter: (page) => page !== "/admin",
-    }),
-    partytown({
-      config: {
-        forward: ["datalayer.push"],
-      },
-    }),
-    preact({ compat: true }),
-  ],
-
+  integrations: [mdx(), tailwind(), sitemap({
+    filter: page => page !== "/admin"
+  }), partytown({
+    config: {
+      forward: ["datalayer.push"]
+    }
+  }), preact({
+    compat: true
+  }), prefetch({
+    selector: "a[href^='/']"
+  })],
   markdown: {
     gfm: true,
     shikiConfig: {
-      theme: customTheme,
+      theme: customTheme
     },
     rehypePlugins: [
-      // This adds links to headings
-      [rehypeAutolinkHeadings, autolinkConfig],
-    ],
-  },
+    // This adds links to headings
+    [rehypeAutolinkHeadings, autolinkConfig]]
+  }
 });
