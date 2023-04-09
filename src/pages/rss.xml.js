dab983e48711a692f19086a4a91b3eedabd2e2b6
Ai Anshu <anshumeena1947@gmail.com>
2023-04-09 09:51:43 +0530

    added: schemas

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,21 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function get() {
+  const blog = await getCollection('blog');
   return rss({
     title: "CodeNanshu - developer's coding blog",
     description:
       "codenanshu is the coding & programming blog that write about web development, artifical intelligence and machine learning. help you to understand computers",
     site: "https://codenanshu.in/",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.mdx")),
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
